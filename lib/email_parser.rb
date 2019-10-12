bab3ccae0ad30d827f5d87d3649c6035eb5b245b
Alexander Bispo <alexbispo173@gmail.com>
2019-10-12 22:03:54 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,12 +1,13 @@
 class EmailAddressParser
-attr_accessor :name, :csv_emails
-def initialize(csv_emails)
-  @csv_emails = csv_emails
-end
+  attr_accessor :name, :csv_emails
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
+  end
 
 def parse
     csv_emails.split.collect do |address|
       address.split(", ")
   end
   .flatten.uniq
+end
 end
